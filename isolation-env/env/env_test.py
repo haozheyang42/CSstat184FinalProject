32ed1798b0cde6bf69e5771aeb30b9641aae367f
SY Wei <shuyangwei@college.harvard.edu>
2024-12-04 01:45:10 -0500

    cleaned

--- isolation-env/env/env_test.py
+++
@@ -1,9 +0,0 @@
-from isolation_env import env
-from pettingzoo.test import api_test, render_test
-
-if __name__ == "__main__":
-    _env = env()
-    api_test(_env, num_cycles=1000, verbose_progress=False)
-
-    env_func = env
-    render_test(env_func)
